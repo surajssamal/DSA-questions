a35f564ff0783427f611176e256d6df3fda286da
suraj samal <surajssamal@gmial.com>
2025-10-02 16:47:03 +0530

    print vector

---
+++ DSA_questions/print_array.h
@@ -0,0 +1,11 @@
+
+// print_container.h
+#pragma once
+
+#include <iostream>
+
+template <typename Container> void print_container(const Container &c) {
+  for (const auto &elem : c)
+    std::cout << elem << " ";
+  std::cout << std::endl;
+}
