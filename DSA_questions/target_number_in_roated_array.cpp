3e6c84dca83e6ace4a72acd80e5bcd7cb037bafc
suraj samal <surajssamal@gmial.com>
2025-10-24 15:05:04 +0530

    target_number in rotation arrya

---
+++ DSA_questions/target_number_in_roated_array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int search_num(vector<int> &input, int target) {
+  int left = 0;
+  int right = input.size() - 1;
+
+  // while (left<=right) {
+  //
+  // }
+  return 1;
+}
+
+int main() {
+
+  vector<int> input = {3, 4, 5, 6, 1, 2};
+  return 0;
+}
