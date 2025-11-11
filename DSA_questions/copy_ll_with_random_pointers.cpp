66e2bbe78f499a903d9f9d374e8992a81199d0c2
suraj samal <surajssamal@gmial.com>
2025-11-11 14:28:12 +0530

    copy linked list with random pointers

--- DSA_questions/copy_ll_with_random_pointers.cpp
+++ DSA_questions/copy_ll_with_random_pointers.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
-int main() {
-  vector<int> something;
-  return 0;
-}
+struct Node {
+  int val;
+  Node *random;
+  Node *next;
+
+  Node() : val(), random(), next() {};
+  Node(int x) : val(x), random(), next() {};
+  Node(int x, Node *n) : val(x), random(), next(n) {};
+  Node(int x, Node *n, Node *r) : val(x), random(r), next(n) {};
+};
+
+Node *copyRandomList(Node *head) {
+  unordered_map<Node *, Node *> record;
+  Node *curr = head;
+  while (curr) {
+    Node *temp = new Node(curr->val);
+    record[curr] = temp;
+    curr = curr->next;
+  }
+  curr = head;
+  while (curr) {
+    Node *copy = record[curr];
+    copy->next = record[curr->next];
+    copy->random = record[curr->random];
+    curr = curr->next;
+  }
+  return record[head];
+
+  int main() { return 0; }
